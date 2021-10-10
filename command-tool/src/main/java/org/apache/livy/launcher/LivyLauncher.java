dc1cc84196cf9534ace3b42d753f39d12db1927a
Jianzhen Wu <jianzhen.wu@shopee.com>
2021-10-10 22:42:49 +0800

    SPDI-16256. Livy CLI entry main function and startup script.

---
+++ command-tool/src/main/java/org/apache/livy/launcher/LivyLauncher.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.livy.launcher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.livy.launcher.exception.LivyLauncherException;
+import org.apache.livy.launcher.runner.PysparkRunner;
+import org.apache.livy.launcher.runner.SparkShellRunner;
+import org.apache.livy.launcher.runner.SparkSqlRunner;
+import org.apache.livy.launcher.runner.SparkSubmitRunner;
+
+public class LivyLauncher {
+
+  private static final Logger logger =
+      LoggerFactory.getLogger(LivyLauncher.class);
+
+  public static void main(String[] args) {
+
+    String sparkCommand = args[0];
+
+    List<String> sparkArgs = new ArrayList<>();
+    if (args.length > 1) {
+      sparkArgs.addAll(Arrays.asList(args).subList(1, args.length));
+    } else {
+      // Default to print usage
+      sparkArgs.add("--help");
+    }
+
+    try {
+      switch (sparkCommand) {
+        case "spark-submit":
+          SparkSubmitOption sparkSubmitOption =
+              new SparkSubmitOption(sparkArgs);
+          SparkSubmitRunner sparkSubmitRunner =
+              new SparkSubmitRunner(sparkSubmitOption);
+          sparkSubmitRunner.run();
+          break;
+        case "spark-sql":
+          SparkSqlOption sparkSqlOption = new SparkSqlOption(sparkArgs);
+          SparkSqlRunner sparkSqlRunner = new SparkSqlRunner(sparkSqlOption);
+          sparkSqlRunner.interactive();
+          break;
+        case "pyspark":
+          PySparkOption pySparkOption = new PySparkOption(sparkArgs);
+          PysparkRunner pysparkRunner = new PysparkRunner(pySparkOption);
+          pysparkRunner.interactive();
+          break;
+        case "spark-shell":
+          SparkShellOption sparkShellOption = new SparkShellOption(sparkArgs);
+          SparkShellRunner sparkShellRunner =
+              new SparkShellRunner(sparkShellOption);
+          sparkShellRunner.interactive();
+          break;
+        default:
+          logger.error("Unsupported sparkCommand {}", sparkCommand);
+          System.exit(1);
+      }
+    } catch (LivyLauncherException e) {
+      logger.info(e.getMessage());
+      System.exit(e.getExitCode().getCode());
+    }
+  }
+}
